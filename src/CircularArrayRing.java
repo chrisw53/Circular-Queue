16ea587d6199389a594ccfce9c75f4f48c662718
Chris Wang <chris_wang@icloud.com>
2018-03-06 00:06:29 +0000

    some fixes

--- src/CircularArrayRing.java
+++ src/CircularArrayRing.java
@@ -1,40 +1,50 @@
 import java.util.*;
+import java.lang.*;
 
-public class CircularArrayRing<Integer> extends AbstractCollection implements Ring {
+public class CircularArrayRing<T> extends AbstractCollection implements Ring {
     private int size;
-    private int[] myArray = new int[0];
+    private T[] myArray = (T[])new Object[0];
     private int addCounter = 0;
     private boolean full = false;
+
+    public CircularArrayRing () {
+        this.size = 1;
+    }
 
     public CircularArrayRing (int size) {
         this.size = size;
     }
 
-    public void add(int value) {
-        if ((myArray.length == 0 || addCounter < size) && !full) {
+    @Override
+    public boolean add(Object value) {
+        if (addCounter < size && !full) {
             myArray = Arrays.copyOf(
                     myArray,
                     myArray.length + 1
             );
-            myArray[addCounter] = value;
+            myArray[addCounter] = (T)value;
             addCounter++;
             if (addCounter == size) {
                 full = true;
                 addCounter = 0;
             }
         } else {
-            myArray[addCounter] = value;
+            if (addCounter == size) {
+                addCounter = 0;
+            }
+
+            myArray[addCounter] = (T)value;
             addCounter++;
         }
+        return true;
     }
 
-    public Integer get(int index) {
+    public T get(int index) {
         int actualIndex = myArray.length - 1 - index;
         if (actualIndex > myArray.length - 1) {
             throw new IndexOutOfBoundsException("Index " + actualIndex + "is out of bound");
         } else {
-            Integer output = new Integer(myArray[actualIndex]);
-            return output;
+            return myArray[actualIndex];
         }
     }
 
@@ -47,5 +57,16 @@ public class CircularArrayRing<Integer> extends AbstractCollection implements Ri
 
     public int size() {
         return myArray.length;
+    }
+
+    public static void main(String[] args) {
+        Ring<Integer> ring = new CircularArrayRing<Integer>(10);
+        for(int i=0; i<12; ++i) {
+            ring.add(i);
+            for(int j=0; j<ring.size(); ++j) {
+                int expected = i-j;
+                int result = ring.get(j);
+            }
+        }
     }
 }
